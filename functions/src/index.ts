2aedf643feebc3845b79711e5129c55046cddfa2
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-04-02 20:31:08 -0500

    Se hace conexion a la base de datos y se agrega servicio que recupera la data de la base de datos de firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,6 +7,8 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+const db = admin.firestore();
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -14,4 +16,15 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.json({
       message: 'Hello from Firebase!!!'
   });
+});
+
+export const getGoty = functions.https.onRequest(async (request, response) => {
+  // const nombre = request.query.nombre || 'Sin Nombre';
+  const gotyRef = db.collection('goty');
+  const docsSnap = await gotyRef.get();
+  const juegos = docsSnap.docs.map(
+    doc => doc.data()
+  );
+
+  response.json(juegos);
 });
