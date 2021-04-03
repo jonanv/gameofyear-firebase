ce2686802038f7cb1fa6f5031f7336fd42a18d72
Johanny Vargas González <jonan-vargas23@hotmail.com>
2021-04-02 20:18:44 -0500

    Se agrega configuracion de la base de datos de farbase con el serviceAccountKey.json y se agrega el .gitignore para que no sea subido

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,16 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
   response.json({
       message: 'Hello from Firebase!!!'
   });
